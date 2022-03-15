a0a4a52adad6d40d632b2c76883599f8b45d07b2
acubillosde <acubillosde@gmail.com>
2022-03-15 06:55:54 -0500

    first commit

---
+++ local/lib/imports.py
@@ -0,0 +1,30 @@
+"""
+Este archivo es generado automaticamente.
+
+###### NO MODIFICAR #########
+
+# cualquier alteración del archivo
+# puede generar una mala calificación o configuracion
+# que puede repercutir negativamente en la 
+# calificación del laboratorio!!!!!
+
+###### NO MODIFICAR #########
+"""
+
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import zero_one_loss
+import matplotlib.pyplot as plt
+import pandas as pd
+from local.lib.general import Tester, Grader, Utils, unknow_error
+from scipy import stats
+import random
+import math
+import numpy.matlib
+import functools
+import scipy.io
+import inspect
+import time
+ut = Utils()
